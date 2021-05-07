1cb5fc636b29f77d06c367d63363a2e3bf3f7123
Alex MIschenko <alexey.mischenko.inc@gmail.com>
2021-05-07 12:02:37 +0300

    Added `LoadingSpinner`

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { Provider } from 'react-redux'
 
 import store from './src/redux/store'
 import RootNavigator from './src/navigator'
+import LoadingSpinner from './src/components/LoadingSpinner'
 
 const App = () => {
   return (
     <Provider store={store}>
       <RootNavigator />
+      {/** TODO: Move `LoadingSpinner` and `NoInternet` to `LoadingContainer` */}
+      <LoadingSpinner />
     </Provider>
   )
 }
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import {
   SafeAreaView,
   View,
@@ -10,16 +11,27 @@ import {
   Alert,
 } from 'react-native'
 
+import { setLoading } from '../../redux/reducers/uiSlice'
 import Routes from '../../pages'
 import cs from './styles'
 
 const Login = ({ navigation, route }) => {
+  const dispatch = useDispatch()
+
   const onSignupPress = () => {
     navigation.navigate(Routes.Signup)
   }
 
   const onForgotPasswordPress = () => {
     Alert.alert('Forgot password', 'Not implemeted yet :)')
+  }
+
+  const handleSubmit = () => {
+    dispatch(setLoading(true))
+    setTimeout(() => {
+      dispatch(setLoading(false))
+      Alert.alert('Fake Login request is complete')
+    }, 1200)
   }
 
   return (
@@ -37,7 +49,7 @@ const Login = ({ navigation, route }) => {
           <Text>Password</Text>
           <TextInput style={cs.formInput} />
         </View>
-        <Pressable style={cs.submitButton}>
+        <Pressable style={cs.submitButton} onPress={handleSubmit}>
           <Text style={cs.submitButtonText}>Log In</Text>
         </Pressable>
       </KeyboardAvoidingView>
