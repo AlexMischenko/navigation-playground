cf1e5ddfae6cb86395f8d763a8567d5ec5686e2d
Alex MIschenko <alexey.mischenko.inc@gmail.com>
2021-05-25 13:09:18 +0300

    Added path aliases setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        root: ['.'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        alias: {
+          '@assets': './assets',
+          '@api': './src/api',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@theme': './src/theme',
+          '@types': './src/types',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 }
