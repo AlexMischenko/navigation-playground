cf0d1bb73bd043164d420ba8c3122aa01d8d4666
Alex MIschenko <alexey.mischenko.inc@gmail.com>
2021-06-03 10:34:04 +0300

    Added unit tests setup -added "@testing-library/react-native" npm package; -added "ts-jest" npm package; -added "tsconfig.jest.json" config; -added "jest-setup.js" file to mock native modules;

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['./jest-setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-(native|universal|navigation)-(.*)|@react-native/(.*)|@react-native-community/(.*)|@react-navigation/(.*)|bs-platform|@rootstrap/redux-tools)',
+  ],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    '\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json',
+      babelConfig: 'babel.config.js',
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
 }
